49ed02f46d829e9669dae66ef06ff72dbedf378c
Karishma Sahu <sahukarishma21@gmail.com>
2023-04-24 12:56:43 +0530

    file

---
+++ Desktop/asss/my-app/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import Invitation from './Invitation';
+
+function App() {
+  return (
+ <Invitation />
+  );
+}
+
+export default App;
---
+++ Desktop/asss/my-app/src/Invitation.js
@@ -0,0 +1,33 @@
+import { Fragment } from "react"
+
+function Invitation(props){
+   
+   const details={
+    Subject: "  Birthday  party Invitation" ,
+    To:"  jaGdish@gmail.com",
+    partyname: " birthday party",
+    otherguest:"Ritu , Saurabh , Kartik",
+    near: "Green field Avenue",
+    name:<h4> jaGdish </h4>
+}
+   //partyname,otherguest,near,from}
+    return(
+        <Fragment>
+    <h4>Subject:{details.Subject}</h4>
+    <h4>To:{details.To}</h4>
+<div> I am having a {details.partyname} next Friday at my Home . Would you like to come ? It will be fun . Lots of people from my school are coming . you know some of them -{details.otherguest}.
+<br></br>
+<br></br>
+my house behind our school , near {details.near} .
+<br></br>
+<br></br>
+I hope you will come and see you soon .
+<br></br>
+from,{details.name}
+
+</div>
+
+    </Fragment>
+    );
+}
+export default Invitation ;
