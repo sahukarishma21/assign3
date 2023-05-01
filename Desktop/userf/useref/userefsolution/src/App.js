067c143f8a0d0142199eed5fb9c190160aa81e28
Karishma Sahu <sahukarishma21@gmail.com>
2023-05-01 19:41:40 +0530

    userefimage

---
+++ Desktop/userf/useref/userefsolution/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import MyComponent from './component/useRef';
+function App() {
+    return (
+     <div className="App">
+      <MyComponent/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Desktop/userf/useref/userefsolution/src/component/useRef.js
@@ -0,0 +1,27 @@
+import { useEffect, useRef, useState } from "react"
+import './userRef.css'
+
+export default function MyComponent() {
+
+    const inputRef = useRef(null)
+
+
+    useEffect(()=> {
+    
+    },[])
+    
+    function handleClick () {
+        inputRef.current.click()
+    }
+    return (
+        <>
+          <div className="App">
+          <button onClick={handleClick}>Upload Image</button>
+          <input ref={inputRef} type="file" />
+          </div>
+        </>
+      
+
+  )
+}
+
