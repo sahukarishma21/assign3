5996da231240008b8ad8c9478530faf65499a99d
Karishma Sahu <sahukarishma21@gmail.com>
2023-05-03 23:31:30 +0530

    newgen

---
+++ Desktop/otpgenerate/generateotp/otpgenerate/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import OtpGen from './component/generateotp'
+function App() {
+  return (
+    <div>
+    <OtpGen/>  
+    </div>
+  )
+}
+
+export default App
+
+
+//otp
