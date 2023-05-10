e3c9467b3795e05de1be15569cbf9617bfa8994b
Karishma Sahu <sahukarishma21@gmail.com>
2023-05-10 20:04:51 +0530

    newcard

---
+++ Desktop/card css/createcard/cardassign/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Card from './Component/Molecule/Scrollbar'
+function App() {
+  return (
+    <div>
+     <Card/> 
+    </div>
+  )
+}
+
+export default App
---
+++ Desktop/card css/createcard/cardassign/src/Component/Molecule/Scrollbar.js
@@ -0,0 +1,122 @@
+import styles from './Scrollbar.module.css'
+
+export default function Card () {
+    
+
+
+    return (
+        <div className={styles.wrapper}>
+            <div className={styles.card}>
+            <img
+                src='https://images.pexels.com/photos/11432781/pexels-photo-11432781.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+                height = '100px'
+                width = '100px'
+                style={{borderRadius : '50%'}}
+               
+            />
+            <h4>Tetiana Bozhakovska</h4>
+            <p>Ui Ux Designer</p>
+           
+            </div>
+            <div className={styles.card}>
+            <img
+                src='https://images.pexels.com/photos/15332188/pexels-photo-15332188.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+                height = '100px'
+                width = '100px'
+                style={{borderRadius : '50%'}}
+            />
+            <h4>Rizky Sabriansyah</h4>
+            <p>Software Engineer</p>
+           
+            </div>
+            <div className={styles.card}>
+            <img
+                src='https://images.pexels.com/photos/15037536/pexels-photo-15037536.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+                height = '100px'
+                width = '100px'
+                style={{borderRadius : '50%'}}
+            />
+            <h4>Marty</h4>
+            <p>App Developer</p>
+         
+            </div>
+            <div className={styles.card}>
+            <img
+                src='https://images.pexels.com/photos/16645005/pexels-photo-16645005.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' 
+                height = '100px'
+                width = '100px'
+                style={{borderRadius : '50%'}}
+            />
+            <h4>David Kouakou</h4>
+            <p>Business associate</p>
+            </div>
+            <div className={styles.card}>
+            <img
+                src='https://images.pexels.com/photos/15598315/pexels-photo-15598315.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+                height = '100px'
+                width = '100px'
+                style={{borderRadius : '50%'}}
+            />
+            <h4>Ayman Mustapha Nouas</h4>
+            <p>Graphic Designer</p>
+            </div>
+            <div className={styles.card}>
+            <img
+                src='https://images.pexels.com/photos/1630305/pexels-photo-1630305.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+                height = '100px'
+                width = '100px'
+                style={{borderRadius : '50%'}}
+            />
+            <h4>Valeria Boltneva</h4>
+            <p>Software Developer</p>
+            
+            </div>
+            <div className={styles.card}>
+            <img
+                src='https://images.pexels.com/photos/7063784/pexels-photo-7063784.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+                height = '100px'
+                width = '100px'
+                style={{borderRadius : '50%'}}
+            />
+            <h4>Barun Sharma</h4>
+            <p>Operation Executive</p>
+            
+            </div>
+            <div className={styles.card}>
+            <img
+                src='https://images.pexels.com/photos/4041089/pexels-photo-4041089.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+                height = '100px'
+                width = '100px'
+                style={{borderRadius : '50%'}}
+            />
+            <h4>Bikram Swain</h4>
+            <p>PHP Developer</p>
+            
+            </div>
+            <div className={styles.card}>
+            <img
+                src='https://images.pexels.com/photos/4243034/pexels-photo-4243034.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+                height = '100px'
+                width = '100px'
+                style={{borderRadius : '50%'}}
+            />
+            <h4>vicky</h4>
+            <p>Mobile Developer</p>
+            
+            </div>
+            <div className={styles.card}>
+            <img
+                src='https://images.pexels.com/photos/3771807/pexels-photo-3771807.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+                height = '100px'
+                width = '100px'
+                style={{borderRadius : '50%'}}
+            />
+            <h4>Andrea Piacquadio</h4>
+            <p>Software Engineer</p>
+            
+            </div>
+           
+                
+        </div>
+    )
+}
