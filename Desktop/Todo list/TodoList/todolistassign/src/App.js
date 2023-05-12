d96e41c47c4cd9014e058673ae3f03ef377e36ad
Karishma Sahu <sahukarishma21@gmail.com>
2023-05-12 22:32:01 +0530

    add

---
+++ Desktop/Todo list/TodoList/todolistassign/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Todolist from './component/molecule/Todolist';
+function App() {
+  return (
+    <div className="App">
+      <span className='title'><b>Todo List</b></span>
+      <Todolist/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Desktop/Todo list/TodoList/todolistassign/src/component/molecule/Todolist.js
@@ -0,0 +1,66 @@
+import React, { useState } from 'react'
+import './Todolist.css';
+function Todolist() {
+    const[task,setTask]=useState("");
+    const[tasklist,setTasklist]=useState([]);
+    const handlechanging= (e) => {
+        setTask(e.target.value);
+    };
+    const AddTask=() => {
+      if(task!==""){
+        const taskDetails= {
+            id:Math.floor(Math.random()*1000),
+            value: task,
+            isCompleted:false,
+        }
+        setTasklist([...tasklist,taskDetails])
+      }
+
+    }
+    const deletetask= (e, id) =>{
+        e.preventDefault();
+        setTasklist(tasklist.filter((t) => t.id != id));
+    };
+    const taskcompleted=(e,id) =>{
+          e.preventDefault();
+
+        const element =tasklist.findIndex(ele=>ele.id==id);
+        const newTasklist=[...tasklist]
+
+        newTasklist[element] ={
+            ...newTasklist[element],
+              isCompleted:true,
+    }
+     setTasklist(newTasklist);
+    }
+  return (
+  <div className='todo'>
+    <input type='text'
+     name='text' 
+     id='text'
+     onChange={e => handlechanging(e)}
+      placeholder='Add task here ...'
+      />
+    <button className='add-btn'onClick={AddTask}>Add</button>
+  <br/>
+  {tasklist!== [] ?(
+  <ul>
+    {tasklist.map((t)=>(
+       <li className={t.isCompleted ? "crossText":"listitem"}>
+        {t.value}
+
+        <button className='completed'onClick={e =>taskcompleted(e,t.id)}>Completed</button>
+        
+        <button className='delete'onClick={e =>deletetask(e,t.id)}>Delete</button>
+        
+       </li> 
+       
+        ))}
+  </ul>
+  
+ ): null}
+  </div>
+  );
+}
+
+export default Todolist
