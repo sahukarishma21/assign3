b00d136284c2fbd28c56e02bf514e078a1b059b7
Karishma Sahu <sahukarishma21@gmail.com>
2023-05-23 01:08:03 +0530

    autosearch

---
+++ src/component/Searchbar/Searchbar.js
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import { FaSearch } from "react-icons/fa";
+import { CgMic } from "react-icons/cg";
+import "./Searchbar.css";
+import SearchResults from "../Searchlist/Searchlist";
+
+export default function Search() {
+  const [input, setInput] = useState("");
+  const [results, setResult] = useState([]);
+  function FetchData(value) {
+    fetch("https://cdn-api.co-vin.in/api/v2/admin/location/states")
+      .then((res) => res.json())
+      .then((deta) => deta.states)
+      .then((states) => {
+        const results = states.filter((users) => {
+          return users && users.state_name.toLowerCase().includes(value);
+        });
+        setResult(results);
+      });
+  }
+
+  function handleChange(value) {
+    setInput(value);
+    FetchData(value);
+  }
+  return (
+    <div className="searchBar">
+     
+      <div className="inputSearchBar">
+        <FaSearch id="searchicon" />
+        
+        <input
+          placeholder="Search or type..."
+          value={input}
+          onChange={(e) => handleChange(e.target.value)}
+          
+        /><CgMic className="mic"/>
+      </div>
+      <SearchResults results={results} />
+    </div>
+  );
+}
